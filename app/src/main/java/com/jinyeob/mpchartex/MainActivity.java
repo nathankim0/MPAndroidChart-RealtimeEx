33711af49b4024f556a0bd39c0d63fe2abd78a55
Jinyeob <jinyeob07@gmail.com>
2020-06-01 14:48:46 +0900

    init

---
+++ app/src/main/java/com/jinyeob/mpchartex/MainActivity.java
@@ -0,0 +1,116 @@
+package com.jinyeob.mpchartex;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+
+public class MainActivity extends AppCompatActivity {
+
+    private LineChart chart;
+    private Thread thread;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        chart = (LineChart) findViewById(R.id.chart);
+
+        chart.getXAxis().setPosition(XAxis.XAxisPosition.BOTTOM);
+
+        chart.getAxisRight().setEnabled(false);
+        chart.getLegend().setTextColor(Color.WHITE);
+        chart.animateXY(2000, 2000);
+        chart.invalidate();
+
+        LineData data = new LineData();
+        chart.setData(data);
+
+        feedMultiple();
+    }
+
+    private void addEntry() {
+        LineData data = chart.getData();
+        if (data != null) {
+            ILineDataSet set = data.getDataSetByIndex(0);
+
+            if (set == null) {
+                set = createSet();
+                data.addDataSet(set);
+            }
+
+            data.addEntry(new Entry(set.getEntryCount(), (float) (Math.random() * 40) + 30f), 0);
+            data.notifyDataChanged();
+
+            chart.notifyDataSetChanged();
+            chart.setVisibleXRangeMaximum(10);
+            chart.moveViewToX(data.getEntryCount());
+        }
+    }
+
+    private LineDataSet createSet() {
+
+        LineDataSet set = new LineDataSet(null, "Dynamic Data");
+        set.setFillAlpha(110);
+        set.setFillColor(Color.parseColor("#d7e7fa"));
+        set.setColor(Color.parseColor("#0B80C9"));
+        set.setCircleColor(Color.parseColor("#FFA1B4DC"));
+        set.setCircleColorHole(Color.BLUE);
+        set.setValueTextColor(Color.WHITE);
+        set.setDrawValues(false);
+        set.setLineWidth(2);
+        set.setCircleRadius(6);
+        set.setDrawCircleHole(false);
+        set.setDrawCircles(false);
+        set.setValueTextSize(9f);
+        set.setDrawFilled(true);
+
+        set.setAxisDependency(YAxis.AxisDependency.LEFT);
+        set.setHighLightColor(Color.rgb(244, 117, 117));
+
+        return set;
+    }
+
+    private void feedMultiple() {
+        if (thread != null)
+            thread.interrupt();
+
+        final Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                addEntry();
+            }
+        };
+
+        thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    runOnUiThread(runnable);
+                    try {
+                        Thread.sleep(3000);
+                    } catch (InterruptedException ie) {
+                        ie.printStackTrace();
+                    }
+                }
+            }
+        });
+        thread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (thread != null)
+            thread.interrupt();
+    }
+}
